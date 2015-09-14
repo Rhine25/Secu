9b9ac4f3c7bdf47309a4534b1afb1f63ff5d0d3a
Rhine25 <celia.dehors@gmail.com>
2015-09-14 09:16:45 -0700

    Fixed decrypt on XYZ letters

--- caesar_decrypt.c
+++ caesar_decrypt.c
@@ -1,3 +1,5 @@
+/* Dans le plaintext, termine par un ; qui sort d'on ne sait où */
+
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]) {
@@ -6,8 +8,14 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	int i;
+	unsigned char c;
 	while ((i = fgetc(stdin)) != EOF) {
-		unsigned char c = (unsigned char) ((i-atoi(argv[1])-65) % 26) +65;
+	if(i == ' '){
+			c = i;
+		}
+		else{
+			c = (unsigned char) ((i-atoi(argv[1])-39) % 26) +65;
+		}
 		printf("%c", c);
 	}
 }
