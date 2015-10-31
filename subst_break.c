9fdf471e527c914fd5467bcd9874a2b0b25967ad
Rhine25 <celia.dehors@gmail.com>
2015-10-31 10:23:28 +0100

    Finished subst_break with great colors and stuff

--- subst_break.c
+++ subst_break.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define couleur(param) printf("\033[%sm",param)
+
 #include "tabs.h"
 //TODO finir décrypter afficher le texte décrypté
 int main(int argc, char *argv[]) {
+	couleur("0");
 	int i;
 	const int NB_ALPHA = 26;
 	char chars[NB_ALPHA]; // lettres dans l'ordre d'apparition
@@ -14,7 +17,9 @@ int main(int argc, char *argv[]) {
 	
 	char texte[500000];
 	
-	while ((i = fgetc(stdin)) != EOF) {
+	FILE* fp = fopen("ciphertext","r");
+	while ((i = fgetc(fp)) != EOF) {
+		printf("%c",i);
 		texte[textLength] = i;
 		textLength ++;
 		if(i > 64 && i < 91){
@@ -36,6 +41,7 @@ int main(int argc, char *argv[]) {
 	
 	
 	int c;
+	printf("\n");
 	for(c = 0; c<nb_elem; c++){
 		percentage[c] = ((float)nb_ap[c]*100) / textLength; //on passe le nombre d'apparition en pourcentage
 		printf("%c : %f \n",chars[c],percentage[c]);
@@ -43,55 +49,61 @@ int main(int argc, char *argv[]) {
 	
 	char decrypt[NB_ALPHA];
 	
-	char choice;
-	do{
+	/*do{
 		printf("Manuel ou Automatique ? : ");
-		scanf("%c",&choice);
-	}while(choice != 'M' && choice != 'A');
+		scanf(" %1s",&choice);
+	}while(choice != 'M' && choice != 'A');*/
 	
-	/*if(percentage[0] <= 16.0f && percentage[0] >= 12.0f){
-		decrypt[0] = 'E';
-		printf("La lettre avec le plus grand pourcentage d'apparition est décryptée en E\n");
+	for(c = 0; c<26; c++){
+		decrypt[c]='.';
 	}
 	
-	decrypt[1] = 'S';
-	decrypt[2] = 'A';
-	decrypt[3] = 'I';
-	decrypt[4] = 'T';
-	decrypt[5] = 'N';
-	decrypt[6] = 'R';
-	decrypt[7] = 'U';
-	decrypt[8] = 'L';
-	decrypt[9] = 'O';
-	decrypt[10] = 'D';
-	decrypt[11] = 'C';
-	decrypt[12] = 'P';
-	decrypt[13] = 'M';
-	decrypt[14] = 'V';
-	decrypt[15] = 'Q';
-	decrypt[16] = 'F';
-	decrypt[17] = 'B';
-	decrypt[18] = 'G';
-	decrypt[19] = 'H';
-	decrypt[20] = 'J';
-	decrypt[21] = 'X';
-	decrypt[22] = 'Y';
-	decrypt[23] = 'Z';
-	decrypt[24] = 'W';
-	decrypt[25] = 'K';*/
-	
-	//demander à l'utilisateur de choisir les lettres correspondantes
-	
-	/*for(c = 0; c < textLength; c++){
-		//printf("%d\n", texte[c]);
-		if(texte[c] < 64 || texte[c] > 91){
-			printf("%c",texte[c]);
+	char choiceE;
+	char choiceD;
+	int cpt = 0;
+	do{
+		cpt++;
+		
+		for(c = 0; c < textLength; c++){
+			if(texte[c] < 64 || texte[c] > 91){
+				printf("%c",texte[c]);
+			}
+			else{
+				if(decrypt[isintab(texte[c], chars, 26)] == '.'){
+					couleur("41");
+					printf("%c",texte[c]);
+					couleur("0");
+				}
+				else{
+					//couleur("44");
+					printf("%c",decrypt[isintab(texte[c], chars, 26)]);
+					couleur("0");
+				}
+			}
 		}
-		else{
-			printf("%c",decrypt[isintab(texte[c], chars, 26)]);
+		
+		printf("\nChoisis une lettre du texte puis la lettre qui doit lui etre associee : \n");
+		scanf(" %c",&choiceE);
+		scanf(" %c",&choiceD);
+		printf("decrypt[%d] = %c\n",isintab(choiceE,chars,26),choiceD);
+		decrypt[isintab(choiceE,chars,26)] = choiceD;
+		
+		//demander à l'utilisateur de choisir les lettres correspondantes
+		
+		for(c = 0; c < textLength; c++){
+			if(texte[c] < 64 || texte[c] > 91){
+				printf("%c",texte[c]);
+			}
+			else{
+				printf("%c",decrypt[isintab(texte[c], chars, 26)]);
+			}
 		}
-	}*/
-	printf("\n");
+		printf("\n");
+	}while(cpt < 26);
+	
+	
+	fclose(fp);
+	couleur("0");
 	return 0;
 	
 }
